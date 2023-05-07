798884810bda6b741e9de0ca29f125935eda70c0
santisan <125523558+sntiwey@users.noreply.github.com>
2023-05-07 10:01:23 -0600

    Add files via upload

---
+++ Main Punto extra 3.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        //Ventana ventana = new Ventana("Layouts");
+        VentanaGris ventanaGris = new VentanaGris("Ventana Layout cuadricula");
+    }
+}
